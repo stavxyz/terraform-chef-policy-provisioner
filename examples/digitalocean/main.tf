019e0bd8d6771f40b289baccc9b094ca61951571
Sam Stavinoha <smlstvnh@gmail.com>
2020-07-15 01:23:45 -0500

    remove cruft

--- examples/digitalocean/main.tf
+++ examples/digitalocean/main.tf
@@ -21,7 +21,7 @@ resource "local_file" "policyfile" {
     name 'chef_client'
     default_source :supermarket
     cookbook 'chef-client', '~> 11.5.0', :supermarket
-    run_list 'chef-client::default', 'managed_chef_server::managed_organization'
+    run_list 'chef-client::default'
   EOT
 }
 
