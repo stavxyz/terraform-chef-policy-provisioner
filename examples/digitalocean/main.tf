c7f9169d66107692b437a211f2886d6423e73f1a
Sam Stavinoha <smlstvnh@gmail.com>
2020-07-16 12:14:55 -0500

    write ssh key for future use by user

--- examples/digitalocean/main.tf
+++ examples/digitalocean/main.tf
@@ -1,6 +1,14 @@
 resource "tls_private_key" "default" {
-  algorithm   = "RSA"
+  algorithm = "RSA"
 }
+
+resource "local_file" "private_key" {
+  filename             = "${path.cwd}/.ssh/${format("%s.pem", tls_private_key.default.public_key_fingerprint_md5)}"
+  sensitive_content    = tls_private_key.default.private_key_pem
+  file_permission      = "0600"
+  directory_permission = "0700"
+}
+
 
 resource "digitalocean_ssh_key" "default" {
   name       = "default"
@@ -8,10 +16,10 @@ resource "digitalocean_ssh_key" "default" {
 }
 
 resource "digitalocean_droplet" "chef-node" {
-  image  = "ubuntu-18-04-x64"
-  name   = "chef-server"
-  region = "nyc2"
-  size   = "s-1vcpu-1gb"
+  image    = "ubuntu-18-04-x64"
+  name     = "chef-server"
+  region   = "nyc2"
+  size     = "s-1vcpu-1gb"
   ssh_keys = [digitalocean_ssh_key.default.fingerprint]
 }
 
@@ -30,4 +38,8 @@ module "provision" {
   host       = digitalocean_droplet.chef-node.ipv4_address
   policyfile = local_file.policyfile.filename
   ssh_key    = tls_private_key.default.private_key_pem
+}
+
+output "droplet_ip" {
+  value = digitalocean_droplet.chef-node.ipv4_address
 }
